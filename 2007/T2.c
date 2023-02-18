645534d84537142ec77eba65946479d1c7aa502c
gowi <919233922@qq.com>
2023-02-18 16:13:42 +0800

    add 2007T2

--- 2007/T2.c
+++ 2007/T2.c
@@ -4,3 +4,16 @@
 
 //编程将一个实数分解为它的整数和小数部分。
 
+#include <stdio.h>
+
+int main()
+{
+	float a, c;
+	int b;
+	scanf("%f", &a);
+	b = a;//隐性类型转换
+	c = a - b;
+	printf("整数部分:%d\n", b);
+	printf("小数部分:%f\n", c);
+	return 0;
+}
