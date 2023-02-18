ad52b9025a9ca0f91452f483463aadab0e587469
gowi <919233922@qq.com>
2023-02-19 00:39:39 +0800

    add 2007T5 Replace with c++

---
+++ 2007/T5.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 伍高巍 on 2023/2/19.
+//
+
+//以二叉链表为存储结构，对二叉树进行层次遍历(层次遍历的定义见6.13).提示：应使用队列来保存各层的结点。
+
+#define N 6
+
+#include "../myTree.h"
+
+int main()
+{
+	int pre[N] = {1, 2, 4, 3, 6, 7}, in[N] = {2, 4, 1, 6, 7, 3}, l1 = 0, l2 = 0, r1 = N - 1, r2 = N - 1;
+	BiTree<int> root = BiTree<int>(pre, in, l1, r1, l2, r2);
+	root.getLevelOrder();
+	return 0;
+}
